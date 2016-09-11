f5dabe46541c12712384b3d41ddba129276faff8
MahamEjaz <maham.ejaz01@gmail.com>
2016-09-12 01:11:44 +0500

    Initial Commit

---
+++ worldDoomedTests/worldDoomedTests.swift
@@ -0,0 +1,36 @@
+//
+//  worldDoomedTests.swift
+//  worldDoomedTests
+//
+//  Created by Maham Ejaz on 12/09/2016.
+//  Copyright © 2016 Maham Ejaz. All rights reserved.
+//
+
+import XCTest
+@testable import worldDoomed
+
+class worldDoomedTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
